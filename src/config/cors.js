12a0a41f67bf3af464a3afe6821e6de312b6d8a2
Ana Beatriz <anabiawalker@gmail.com>
2021-12-27 17:44:04 -0300

    Habilitando o Cors

---
+++ src/config/cors.js
@@ -0,0 +1,11 @@
+//Criando um middleware
+
+module.exports = function(req, res, next) {
+    //Adicionando cabeçalhos nas respostas que serão enviadas pro browser pra dizer que é permitida acessar o serviço solicitado
+    res.header('Access-Control-Allow-Origin', '*')
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE')
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
+    
+    //Continua o fluxo da aplicação, indo para o próximo middleware
+    next()
+}
